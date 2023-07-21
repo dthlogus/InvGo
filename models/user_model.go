6d89dbf53ddf23ca3ad653f097cec5e3546422de
Matheus Ribeiro <matheus.junio159@gmail.com>
2023-07-20 22:26:05 -0300

    Ajuste para ter apenas um campo ID

--- models/user_model.go
+++ models/user_model.go
@@ -3,7 +3,7 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	Id       primitive.ObjectID `json:"id,omitempty"`
+	Id       primitive.ObjectID `json:"_id,omitempty"`
 	Username string             `json:"username,omitempty" validate:"required"`
 	FullName string             `json:"fullname,omitempty" validate:"required"`
 	Password string             `json:"password,omitempty" validate:"required"`
